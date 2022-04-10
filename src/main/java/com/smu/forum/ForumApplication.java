4f6b6b41576474f4bc3a5ab4d3de58be07e0512c
Kehan Zhang <827925170@qq.com>
2022-04-10 10:54:25 -0500

    deploy to app engine

--- src/main/java/com/smu/forum/ForumApplication.java
+++ src/main/java/com/smu/forum/ForumApplication.java
@@ -2,12 +2,18 @@ package com.smu.forum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
+@RestController
 public class ForumApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ForumApplication.class, args);
 	}
 
+	@GetMapping("/")
+	public String hello() {
+		return "hello world!";
+	}
 }
